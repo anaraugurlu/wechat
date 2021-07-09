67d24b3e00e2734f4a7d4b5ee143d4deceaff229
Ugur_ml97 <Ugur_ml97@itstep.edu.az>
2021-07-09 11:05:26 +0400

    Добавьте файлы проекта.

---
+++ wechat/AnswersAndQuestiond.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wechat
+{
+    class AnswersAndQuestiond
+    {
+      public  static string Answers(string answer)
+        {
+            if (answer == "salam")
+            {
+                return "salam";
+            }
+            else if (answer=="necesen?"|| answer == "necesen")
+            {
+                return "yaxshi sen?";
+            }
+            else if(answer=="ne var ne yox?"|| answer == "ne var ne yox")
+            {
+                return "salamatchiliqdi";
+            }
+            else if(answer == "sagol")
+            {
+                return "sagol";
+            }
+            return " ";
+        }
+    }
+}
---
+++ wechat/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wechat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sendbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(messtxtb.Text != null)
+            {
+                Label label = new Label();
+                Label label2 = new Label();
+              
+            //    Label label2 = new Label();
+              //  Label[] labels = { label, label2 };
+                //foreach (var item in labels)
+                //{
+                  //  if(item.Content == null)
+                    //{
+                        label.Content = messtxtb.Text ;
+               label2 .Content = AnswersAndQuestiond.Answers(messtxtb.Text);
+                //}
+                //     }
+                list.Items.Add(label.Content);
+                list2.Items.Add(label2.Content);
+
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            window.Close();
+        }
+
+        private void list2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void list2_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
